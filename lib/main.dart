d7039c4bf17db8b8355f0164d60abfb315e70c4c
knee-son <okaokaloid@gmail.com>
2024-09-03 07:02:09 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WildCats 😺',
+      title: 'WildCats 😺😊',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Colors.amber,
